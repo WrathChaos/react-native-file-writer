aee8c810f32579df80d7c798b965a86a0f42a5b6
FreakyCoder <kurayogun@gmail.com>
2021-11-17 19:33:55 +0300

    Working FileWriter is here

---
+++ example/App.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Alert, SafeAreaView } from "react-native";
+import RNFetchBlob from "rn-fetch-blob";
+import FileWriter from "./lib/FileWriter";
+
+const mockData = [
+  {
+    name: "FreakyCoder123",
+    email: "kurayogun@gmail.com",
+    github: "https://github.com/WrathChaos",
+  },
+  {
+    name: "FreakyCoder123",
+    email: "kurayogun@gmail.com",
+    github: "https://github.com/WrathChaos",
+  },
+  {
+    name: "FreakyCoder123",
+    email: "kurayogun@gmail.com",
+    github: "https://github.com/WrathChaos",
+  },
+];
+
+const App = () => {
+  return (
+    <SafeAreaView
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+    >
+      <FileWriter
+        data={mockData}
+        filename="freakycoder123"
+        fileExtension="json"
+        enableStringify
+        enableDecycle
+        onPress={(path: string) => {
+          Alert.alert("FileWriter", path);
+          RNFetchBlob.android.actionViewIntent(path, "application/json");
+        }}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default App;
